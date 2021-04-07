905f3fc309df74552792b61d25879f1717156fec
Ville Kylmämaa <71127573+VilleKylmamaa@users.noreply.github.com>
2021-04-07 13:33:40 +0300

    Update MemoryGame.ino

--- Arduino Memory Game Code/MemoryGame.ino
+++ Arduino Memory Game Code/MemoryGame.ino
@@ -1,6 +1,6 @@
 
 // Initialize variables
-const byte blue  = 10; 
+const byte blue  = 10;
 const byte red = 11;
 const byte green = 12;
 const byte orange = 13;
@@ -18,12 +18,18 @@ byte sequenceLength = 0;
 byte inputCount = 0;
 byte correctInput = 0;
 byte buttonPressed = 0;
-bool inputGiven = false;
+bool buttonDown = false;
 bool waitUserInput = false;
 
+// Button debouncing is used to counter noise in the button signals
+// Reference: https://www.arduino.cc/en/pmwiki.php?n=Tutorial/Debounce
+// The following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
 void setup() {
-  // Set inputs and outputs
+    // Set inputs and outputs
     pinMode(blue, OUTPUT);
     pinMode(red, OUTPUT);
     pinMode(green, OUTPUT);
@@ -37,7 +43,7 @@ void setup() {
     randomSeed(analogRead(0));
   
     Serial.begin(9600);
-    delay(500);
+    delay(1000);
 }
 
 
@@ -50,9 +56,9 @@ void loop() {
       // Display the sequence to the player
       for (int i = 0; i < sequenceLength; i++) {
         digitalWrite(ledArray[sequence[i]], HIGH);
-        delay(500);
+        delay(400);
         digitalWrite(ledArray[sequence[i]], LOW);
-        delay(200);
+        delay(100);
       }
       
       Serial.print("\n\nSequence length: ");
@@ -63,36 +69,52 @@ void loop() {
         // Get the current correct LED value in the sequence
         correctInput = sequence[inputCount];
 
-        // Check if any button has been pressed
-        for (int i = 0; i < 4; i++) {
-          if (digitalRead(buttonArray[i]) == LOW) {
-            buttonPressed = i;
-            inputGiven = true;
+        // Check if any button has been pressed down
+        if (!buttonDown) {
+          for (int i = 0; i < 4; i++) {
+            // digitalRead() returning LOW means the button is pressed down
+            // because the buttons are set as INPUT_PULLUP
+            if (digitalRead(buttonArray[i]) == LOW) {
+              buttonPressed = i;
+              buttonDown = true;
+            }
           }
         }
 
-        // If the user has pressed a button and released it
-        if (inputGiven && digitalRead(buttonArray[buttonPressed]) == HIGH) {
-          inputGiven = false;
-          // If correct
-          if (buttonPressed == correctInput) {
-            Serial.print("\nCorrect :D");
-            inputCount++;
-            if (inputCount == sequenceLength) {
+        if (buttonDown) {
+          // If the button is still down or there is noise after its release
+          if (digitalRead(buttonArray[buttonPressed]) == LOW) {
+            // reset the debouncing timer
+            lastDebounceTime = millis();
+          }
+
+          // This if statement passes if the button that was pressed
+          // has returned HIGH for longer than the debounce delay
+          if ((millis() - lastDebounceTime) > debounceDelay) {
+              buttonDown = false;
+
+            // If the input was correct, proceed to next input
+            if (buttonPressed == correctInput) {
+              Serial.print("\nCorrect :D");
+              inputCount++;
+              // If that was the last input of the sequence, proceed to next sequence
+              if (inputCount == sequenceLength) {
                 inputCount = 0;
-              waitUserInput = false;
+                waitUserInput = false;
                 delay(500);
+              }
+
+            // If the input was wrong, the game is over and a new game starts after some delay
+            } else {
+              Serial.print("\nYou lose :(");
+              sequenceLength = 0;
+              inputCount = 0;
+              waitUserInput = false;
+              
+              delay(1000);
+              Serial.print("\n\n\n-- New game --");
+              delay(1000);
             }
-          // If wrong
-          } else {
-            Serial.print("\nYou lose :(");
-            sequenceLength = 0;
-            inputCount = 0;
-            waitUserInput = false;
-            
-            delay(1000);
-            Serial.print("\n\n\nNew game :3\n");
-            delay(1000);
           }
         }
     }
